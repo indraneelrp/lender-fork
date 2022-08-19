825fd8a400e2cd77dfb917655b16cdf1daeed054
INDR <indraneel.paranjape@gmail.com>
2022-08-19 17:38:32 +0800

    restored commit 2 (should put the exact SHA)

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,7 +3,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("another line");
-        System.out.println("yet another line");
     }
     
 }
